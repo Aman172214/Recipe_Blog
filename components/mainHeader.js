ae20773b62fb307a67c34a0f19492cf1fbbe0f3f
Aman <Sharmaamqn@gmail.com>
2024-10-22 23:37:47 +0530

    create the home page

--- components/mainHeader.js
+++ components/mainHeader.js
@@ -1,25 +1,31 @@
 import Link from "next/link";
 import React from "react";
 import logoImg from "@/assets/logo.png";
+import classes from "./mainHeader.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "./mainHeaderBackground";
 
 const MainHeader = () => {
   return (
-    <header>
-      <Link href="/">
-        <img src={logoImg.src} alt="logo image" />
-        NextLevel Food
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="logo image" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
